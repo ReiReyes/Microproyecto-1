6c89662f5ef7e211285f16e9ffb6d557cc28e6a9
Reinaldo Reyes <reinaldo.reyes@correo.unimet.edu.ve>
2024-05-26 00:15:12 -0400

    Dificultad agregada

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,41 @@ const keyboardDiv = document.querySelector(".keyboard");
 const gameModal = document.querySelector(".game-modal");
 const playAgainBtn = document.querySelector(".play-again");
 const hangman = document.querySelector(".hangman-box");
+const dificulty = document.querySelector(".game-dificulty");
+const facil = document.querySelector(".f");
+const media = document.querySelector(".m");
+const dificil = document.querySelector(".d");
 
-let currentWord, correctLetters, wrongGuessCount, bestpoints, points;
-const maxGuesses = 6;
+let currentWord, correctLetters, wrongGuessCount, bestpoints, points, dificultad ;
+const maxGuessesF = 12;
+const maxGuessesM = 6;
+const maxGuessesD = 4;
 bestpoints = 0;
 points = 0;
 
+
+
 const resetGame = () => {
   //Funcion que le hace reset al juego y su interfaz
-  correctLetters = [];
-  wrongGuessCount = 0;
-  hangmanImage.src = `imagenes/${wrongGuessCount + 1}.jpg`;
-  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  if(dificultad === "facil"){
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `imagenes/${wrongGuessCount + 1}.jpg`;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuessesF}`;
+    
+  }else if(dificultad === "medio"){
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `imagenes/${wrongGuessCount + 7}.jpg`;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuessesM}`;
+  
+  
+  }else if(dificultad === "dificil"){
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `imagenes/${wrongGuessCount + 9}.jpg`;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuessesD}`;
+  }
   keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
   wordDisplay.innerHTML = currentWord.split("").map(() => '<li class="letter"></li>').join("");
   gameModal.classList.remove("show");
@@ -24,12 +47,38 @@ const resetGame = () => {
 
 const getRandomWord = () => {
     //selecciones una palabras random con su pista de wordList
+    
     const{ word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
     console.log(word);
     document.querySelector(".hint-text b").innerText = hint;
     resetGame();
 }
+const gameDificulty = () => { 
+  dificulty.classList.add("show");
+  facil.addEventListener("click", easy);
+  media.addEventListener("click", medium);
+  dificil.addEventListener("click", hard);
+}
+const easy= () => {
+  dificultad = "facil";
+  getRandomWord();
+  hangman.querySelector("h6").innerText= `Dificultad: ${dificultad}`;
+  dificulty.classList.remove("show")
+}
+const medium = () =>{
+  dificultad = "medio";
+  getRandomWord();
+  hangman.querySelector("h6").innerText= `Dificultad: ${dificultad}`;
+  dificulty.classList.remove("show");
+}
+const hard = () =>{
+  dificultad = "dificil";
+  getRandomWord();
+  hangman.querySelector("h6").innerText= `Dificultad: ${dificultad}`;
+  dificulty.classList.remove("show");
+}
+
 
 const gameOver = (isVictory) => {
   // despues de 300ms de que se complete el juego, se muestra la info 
@@ -40,17 +89,49 @@ const gameOver = (isVictory) => {
     gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
     gameModal.classList.add("show");
   }, 300)
+  // suma el puntaje actual si adivino la palabra\
+
+  if(dificultad === "facil"){
+    
+    if(wrongGuessCount !==12){
+      points += (maxGuessesF - wrongGuessCount)/2;
+      hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
+      if(points > bestpoints){
+        bestpoints = points;
+        hangman.querySelector("h5").innerText = `Mejor Puntaje: ${bestpoints} pts`;
+      }
+    }else{
+      points = 0;
+      hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
+    }
+  }else if(dificultad === "medio"){
+    
   if(wrongGuessCount !==6){
-    points += maxGuesses - wrongGuessCount;
+    points += maxGuessesM - wrongGuessCount;
     hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
     if(points > bestpoints){
       bestpoints = points;
       hangman.querySelector("h5").innerText = `Mejor Puntaje: ${bestpoints} pts`;
     }
-  }else{
-    points = 0;
-    hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
-  }
+    }else{
+      points = 0;
+      hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
+    }
+  }else if(dificultad === "dificil"){
+    
+    if(wrongGuessCount !== 4){
+      points += maxGuessesD*3 - wrongGuessCount*2;
+      
+      hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
+      if(points > bestpoints){
+        bestpoints = points;
+        hangman.querySelector("h5").innerText = `Mejor Puntaje: ${bestpoints} pts`;
+      }
+    }else{
+      
+      points = 0;
+      hangman.querySelector("h3").innerText = `Puntaje Actual: ${points} pts`;
+    }}
 }
 
 
@@ -69,15 +150,35 @@ const initGame = (button, clickedLetter) => {
   else{
     //Si la letra no es correcta, muestra la siguiente imagen y suma 1 al contador.
     wrongGuessCount++;
-    hangmanImage.src = `imagenes/${wrongGuessCount + 1}.jpg`;
+    if(dificultad === "facil"){
+      hangmanImage.src = `imagenes/${wrongGuessCount + 1}.jpg`;
+    }else if (dificultad === "medio"){
+      hangmanImage.src = `imagenes/${wrongGuessCount+7}.jpg`;
+    }else if (dificultad === "dificil"){
+      hangmanImage.src = `imagenes/${wrongGuessCount+9}.jpg`;
+    }
+    
   }
 
   button.disabled = true;
-  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  if(dificultad === "facil"){
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuessesF}`;
+    //Funcion del GameOver
+    if(wrongGuessCount === maxGuessesF) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
 
-  //Funcion del GameOver
-  if(wrongGuessCount === maxGuesses) return gameOver(false);
-  if(correctLetters.length === currentWord.length) return gameOver(true);
+  }else if(dificultad === "medio"){
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuessesM}`;
+    //Funcion del GameOver
+    if(wrongGuessCount === maxGuessesM) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
+
+  }else if(dificultad === "dificil"){
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuessesD}`;
+    //Funcion del GameOver
+    if(wrongGuessCount === maxGuessesD) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
+  }
 }
 
 //Crea los botones de las letras y tiene el event listener
@@ -88,6 +189,5 @@ for (let index = 97; index <= 122; index++) {
     button.addEventListener("click", e => initGame(e.target, String.fromCharCode(index)))
 }
 
-
-getRandomWord(); 
+gameDificulty();
 playAgainBtn.addEventListener("click", getRandomWord);
